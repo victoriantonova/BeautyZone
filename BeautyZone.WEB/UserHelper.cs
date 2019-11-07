04d0c73a85c90d745ddde817ddf61bbd28dfe68d
Victoria Antonova <victori.antonova@gmail.com>
2019-11-07 22:23:48 +0300

    User Identity WEB

---
+++ BeautyZone.WEB/UserHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeautyZone.WEB
+{
+    public static class UserHelper
+    {
+        public static string IdUser { get; set; }
+        public static string UserName { get; set; }
+    }
+}
